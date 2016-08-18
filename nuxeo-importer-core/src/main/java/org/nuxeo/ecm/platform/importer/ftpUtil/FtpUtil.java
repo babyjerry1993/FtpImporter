6516b09cc16dc6641b9fdf5118080975be53ffc5
xiaowen.xie <xiaowen.xie@nextcont.com>
2016-08-18 16:52:13 +0800

    解决了FTP批量文档的问题，并新建了仓库来管理，在官方源码子项目上整合FTP功能实现

---
+++ nuxeo-importer-core/src/main/java/org/nuxeo/ecm/platform/importer/ftpUtil/FtpUtil.java
@@ -0,0 +1,136 @@
+package org.nuxeo.ecm.platform.importer.ftpUtil;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPClientConfig;
+import org.apache.commons.net.ftp.FTPFile;
+import org.apache.commons.net.ftp.FTPReply;
+
+public class FtpUtil {
+
+	// 下载文件(单个指定文件)
+	public boolean downloadSingleFromFtp(FtpInfo info) {
+		boolean success = false;
+		FTPClient ftp = new FTPClient();
+		try {
+			int reply;
+			// ftp.connect(url);
+			ftp.connect(info.getUrl(), info.getPort());
+			// 如果采用默认端口，可以使用ftp.connect(url)的方式直接连接FTP服务器
+			ftp.login(info.getUsername(), info.getPassword());// 登录
+			reply = ftp.getReplyCode();// 返回码
+			if (!FTPReply.isPositiveCompletion(reply)) {// reply >= 200 && reply
+														// < 300
+				ftp.disconnect();
+				return success;
+			}
+			ftp.changeWorkingDirectory(info.getRemotePath());// 转移到FTP服务器目录
+			FTPFile[] fs = ftp.listFiles();
+			for (FTPFile ff : fs) {// 遍历ftp远程路径中的文件,匹配文件名的根据localFile输出到本地路径
+				if (ff.getName().equals(info.getFileName())) {
+
+					File localFile = new File(info.getLocalPath() + "/" + ff.getName());
+
+					OutputStream is = new FileOutputStream(localFile);
+					ftp.retrieveFile(ff.getName(), is);// 将获取到的流存盘到本地，完成到下载的操作
+					is.close();
+				}
+			}
+			ftp.logout();// ftp退出登录
+			success = true;
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (ftp.isConnected()) {
+				try {
+					ftp.disconnect();// 操作完成了关闭ftp
+				} catch (IOException ioe) {
+				}
+			}
+		}
+		return success;
+	}
+
+	// 下载文件(批量)
+	public boolean downloadFolderFromFtp(FtpInfo info) {
+		boolean success = false;
+		FTPClient ftp = new FTPClient();
+
+		// 设置中文编码----
+		ftp.setControlEncoding("GBK");
+		FTPClientConfig conf = new FTPClientConfig(FTPClientConfig.SYST_UNIX);
+		conf.setServerLanguageCode("zh");
+		ftp.configure(conf);
+		// -------------
+		try {
+			int reply;
+			ftp.connect(info.getUrl(), info.getPort());
+			// 如果采用默认端口，可以使用ftp.connect(url)的方式直接连接FTP服务器
+			ftp.login(info.getUsername(), info.getPassword());// 登录
+			reply = ftp.getReplyCode();// 返回码
+			if (!FTPReply.isPositiveCompletion(reply)) {// reply >= 200 && reply
+														// < 300
+				ftp.disconnect();
+				return success;
+			}
+			ftp.changeWorkingDirectory(info.getRemotePath());// 转移到FTP服务器目录
+			FTPFile[] fs = ftp.listFiles();
+			for (FTPFile ff : fs) {// 将ftp远程路径中的文件全部输出到本地路径
+				if (ff.isFile()) {
+					File localFile = new File(info.getLocalPath() + "/" + ff.getName());
+					OutputStream is = new FileOutputStream(localFile);
+					ftp.retrieveFile(ff.getName(), is);// 将获取到的流存盘到本地，完成到下载的操作
+					is.close();
+				}
+			}
+			ftp.logout();// ftp退出登录
+			success = true;
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (ftp.isConnected()) {
+				try {
+					ftp.disconnect();// 操作完成了关闭ftp
+				} catch (IOException ioe) {
+				}
+			}
+		}
+		return success;
+	}
+
+	// 下载ftp文件,获取流，用于上传到nuxeo
+	public FileOutputStream getFileStream(FtpInfo info) throws IOException {
+
+		FileOutputStream fos = null;
+		FTPClient ftp = new FTPClient();
+		ftp.connect(info.getUrl(), info.getPort());// 如果采用默认端口，可以使用ftp.connect(url)的方式直接连接FTP服务器
+		ftp.login(info.getUsername(), info.getPassword());// 登录
+		int reply = ftp.getReplyCode();// 返回码
+
+		System.out.println(reply);// 打印返回码
+
+		if (!FTPReply.isPositiveCompletion(reply)) {// 若失败
+			ftp.disconnect();
+			return null;// 返回空
+		}
+		// 若成功
+		ftp.changeWorkingDirectory(info.getRemotePath());// 转移到FTP服务器目录
+		FTPFile[] fs = ftp.listFiles();
+		for (FTPFile ff : fs) {// 遍历ftp远程路径中的文件,匹配文件名的根据localFile输出到本地路径
+			if (ff.getName().equals(info.getFileName())) {
+				File localFile = new File(info.getLocalPath() + "/" + ff.getName());
+				fos = new FileOutputStream(localFile);
+			}
+		}
+		ftp.logout();// ftp退出登录
+		if (ftp.isConnected()) {// 检查ftp连接状态，关闭
+			ftp.disconnect();
+		}
+		return fos;
+	}
+
+}
---
+++ nuxeo-importer-jaxrs/src/main/java/org/nuxeo/ecm/platform/importer/executor/jaxrs/HttpFileImporterExecutor.java
@@ -0,0 +1,188 @@
+/*
+ * (C) Copyright 2009 Nuxeo SA (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Thierry Delprat
+ */
+package org.nuxeo.ecm.platform.importer.executor.jaxrs;
+
+import java.io.File;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.nuxeo.ecm.platform.importer.base.ImporterRunner;
+import org.nuxeo.ecm.platform.importer.ftpUtil.FtpInfo;
+import org.nuxeo.ecm.platform.importer.ftpUtil.FtpUtil;
+import org.nuxeo.ecm.platform.importer.service.DefaultImporterService;
+import org.nuxeo.runtime.api.Framework;
+
+@Path("myImporter")
+public class HttpFileImporterExecutor extends AbstractJaxRSImporterExecutor {
+
+    private static final Log log = LogFactory.getLog(HttpFileImporterExecutor.class);
+
+    protected DefaultImporterService importerService;
+
+    @Override
+    protected Log getJavaLogger() {
+        return log;
+    }
+    
+//    @GET
+//   	@Path("run")
+//   	@Produces("text/plain; charset=UTF-8")
+//   	public String run(@QueryParam("leafType") String leafType, 
+//   			@QueryParam("folderishType") String folderishType,
+//   			@QueryParam("url") String url, 
+//   			@QueryParam("port") String port, 
+//   			@QueryParam("username") String username,
+//   			@QueryParam("password") String password, 
+//   			@QueryParam("path") String path,
+//   			@QueryParam("targetPath") String targetPath,
+//   			@QueryParam("skipRootContainerCreation") Boolean skipRootContainerCreation,
+//   			@QueryParam("batchSize") Integer batchSize,
+//   			@QueryParam("nbThreads") Integer nbThreads,
+//   			@QueryParam("interactive") Boolean interactive,
+//   			@QueryParam("transactionTimeout") Integer transactionTimeout) {
+//
+//   		if (port == null) {
+//   			port = "21";
+//   		}
+//
+//   		if (url == null || username == null || password == null) {
+//   			return "Can not import, missing info of FTP";
+//   		}
+//   		if (path == null || targetPath == null) {
+//   			return "Can not import, missing " + (path == null ? "path" : "targetPath");
+//   		}
+//   		if (skipRootContainerCreation == null) {
+//   			skipRootContainerCreation = false;
+//   		}
+//   		if (batchSize == null) {
+//   			batchSize = 5;
+//   		}
+//   		if (nbThreads == null) {
+//   			nbThreads = 5;
+//   		}
+//   		if (interactive == null) {
+//   			interactive = false;
+//   		}
+//   		if (transactionTimeout == null) {
+//   			transactionTimeout = 0;
+//   		}
+//   		getImporterService().setTransactionTimeout(transactionTimeout);
+//
+//   		// 改变了html填入的参数url(IP)+相对路径，以适应FTP上传批量文件
+//   		// 本地缓存目录，若不存在则建立
+//   		String sourcePath = "C:\temp";
+//   		File file = new File(sourcePath);
+//   		if (!file.exists()) {
+//   			try {
+//   				file.createNewFile();
+//   			} catch (IOException e) {
+//   				e.printStackTrace();
+//   			}
+//   		}
+//
+//   		//做下载到缓存目录的操作
+//   		FtpUtil fu = new FtpUtil();
+//   		FtpInfo info = new FtpInfo(url, Integer.parseInt(port.trim()), username, password, path, null, sourcePath);
+//   		fu.downloadFolderFromFtp(info);
+//
+//
+//   		//导入都是从缓存目录取文件
+//   		if (leafType != null || folderishType != null) {
+//   			log.info("Importing with the specified doc types");
+//   			return getImporterService().importDocuments(this, leafType, folderishType, targetPath, sourcePath,
+//   					skipRootContainerCreation, batchSize, nbThreads, interactive);
+//   		} else {
+//   			log.info("Importing with the deafult doc types");
+//   			return getImporterService().importDocuments(this, targetPath, sourcePath, skipRootContainerCreation,
+//   					batchSize, nbThreads, interactive);
+//   		}
+//
+//   	}
+
+    @GET
+    @Path("run")
+    @Produces("text/plain; charset=UTF-8")
+    public String run(@QueryParam("leafType") String leafType, @QueryParam("folderishType") String folderishType,
+            @QueryParam("inputPath") String inputPath, @QueryParam("targetPath") String targetPath,
+            @QueryParam("skipRootContainerCreation") Boolean skipRootContainerCreation,
+            @QueryParam("batchSize") Integer batchSize, @QueryParam("nbThreads") Integer nbThreads,
+            @QueryParam("interactive") Boolean interactive, @QueryParam("transactionTimeout") Integer transactionTimeout) {
+
+        if (inputPath == null || targetPath == null) {
+            return "Can not import, missing " + (inputPath == null ? "inputPath" : "targetPath");
+        }
+        if (skipRootContainerCreation == null) {
+            skipRootContainerCreation = false;
+        }
+        if (batchSize == null) {
+            batchSize = 5;
+        }
+        if (nbThreads == null) {
+            nbThreads = 5;
+        }
+        if (interactive == null) {
+            interactive = false;
+        }
+        if (transactionTimeout == null) {
+            transactionTimeout = 0;
+        }
+        
+        //创建一个文件夹-----------------------
+        String sourcePath = "C:\\temp";
+		File myFilePath = new File(sourcePath);
+		if (!myFilePath.exists()) {
+			myFilePath.mkdir();
+		}
+		//Ftp批量下载文件到sourcePath
+		FtpUtil fu = new FtpUtil();
+		FtpInfo info = new FtpInfo("169.254.90.39", 21, "shawn", "shawn", "/py", null, sourcePath);
+
+		fu.downloadFolderFromFtp(info);
+		//--------------------------------
+
+        getImporterService().setTransactionTimeout(transactionTimeout);
+
+        if (leafType != null || folderishType != null) {
+            log.info("Importing with the specified doc types");
+            return getImporterService().importDocuments(this, leafType, folderishType, targetPath, inputPath,
+                    skipRootContainerCreation, batchSize, nbThreads, interactive);
+        } else {
+            log.info("Importing with the deafult doc types");
+            return getImporterService().importDocuments(this, targetPath, inputPath, skipRootContainerCreation,
+                    batchSize, nbThreads, interactive);
+        }
+
+    }
+
+    @Override
+    public String run(ImporterRunner runner, Boolean interactive) {
+        return doRun(runner, interactive);
+    }
+
+    protected DefaultImporterService getImporterService() {
+        if (importerService == null) {
+            importerService = Framework.getService(DefaultImporterService.class);
+        }
+        return importerService;
+    }
+}
